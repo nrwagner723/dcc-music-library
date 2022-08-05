9cb26e588cf74ef3d05f9fcd9b985f4c25156d99
Natalie Wagner <nrwagner723@gmail.com>
2022-08-05 15:50:23 -0500

    worked on filtering/search bar functionaility

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar ({songs}) {
+    
+    const [filteredSongs, setFilteredSongs] = useState([]);
+    
+    const handleFilter = (event) => {
+        const searchTerm = event.target.value;
+        const newFitler = songs.filter((value) => {
+            return value.title.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        if (searchTerm === '') {
+            setFilteredSongs([]);
+        } else {
+            setFilteredSongs(newFitler);
+    }}
+
     return ( 
         <div className='search'>
             <div className='searchInputs'>
-                <input type='text' placeholder='Search'/>
-                <div className='searchIcon'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
-                        <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
-                        <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
-                    </svg>
-                </div>
+                <input type='text' placeholder='Search' onChange={handleFilter}/>
             </div>
             <div className='dataResults'>
-                {songs.map((value, key) => {
-                    return <div> {value} </div>;
+                {filteredSongs.map((value, key) => {
+                    return ( 
+                        <div> {value.title} </div> 
+                     );
                 })}
             </div>
         </div>
-     )
+     );
 }
  
 export default SearchBar;
